4b883ef48ec549e6a3cb96db0b2bf314cc5a10cc
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2023-08-13 18:46:18 +0200

    js9-0

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,24 @@
-import axios from 'axios';
+const url = 'https://api.thecatapi.com/v1';
 
-axios.defaults.headers.common['x-api-key'] =
+const api_key =
   'live_O7F7DJLBa1xb9JNdoi7AKiMxVsxumSSfhm3C75reQo8i6eQlRHGdC6Nd1w5jfflf';
+
+export function fetchBreeds() {
+  return fetch(`${url}/breeds?api_key=${api_key}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
